4cb2804080ef6e8d48b869a9916ac761a9c42f94
alexagambo4 <a.23308051281189@cbtis128.edu.mx>
2024-10-30 15:15:26 -0600

    Add files via upload

---
+++ practicando_funciones.py/practiando_funciones14.py
@@ -0,0 +1,11 @@
+#imprime los datos del autor
+print("Jimenez Gamboa Issis Alexa")
+print("No.1189")
+print("3W")
+#imprime una linea en blanco para que se vea mas limpio 
+print("")
+
+def my_function(x):
+    print(x)
+
+my_function(x = 3)
---
+++ practicando_funciones.py/practicando_funciones17.py
@@ -0,0 +1,10 @@
+#imprime los datos del autor
+print("Jimenez Gamboa Issis Alexa")
+print("No.1189")
+print("3W")
+#imprime una linea en blanco para que se vea mas limpio 
+print("")
+def my_function(x):
+    print(x)
+
+my_function(3)
